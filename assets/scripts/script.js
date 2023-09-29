1c85208462b9685d94b7823588d9c6ebf5bde815
Miguel Garrocini <124930647+Edmig1@users.noreply.github.com>
2023-09-28 21:23:46 -0300

    menu cinza

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,14 @@
 menu = document.getElementsByClassName('menu')[0]
+botao = document.getElementsByClassName('botao')[0]
 function abrirmenu(event){
     event.preventDefault()
     menu.classList.remove('fechado')
     menu.classList.add('aberto')
+    botao.style.visibility = 'hidden'
 }
 fm = document.getElementById('fm')
 function fecharmenu(){
     menu.classList.add('fechado')
     menu.classList.remove('aberto')
+    botao.style.visibility = 'visible'
 }
